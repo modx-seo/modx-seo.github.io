7b6721c53aaab8a48f1aabfcf2eb3cf3404b3d92
modx_pro <yarikbb@gmail.com>
2026-01-07 17:22:19 +0200

    11

--- app.js
+++ app.js
@@ -284,3 +284,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', savedTheme);
 });
+
+
